c30c9079e232e0a81c39249d1ce60a77d3b93b60
Joseph Innes <avianpneuma@gmail.com>
2023-11-20 17:49:01 +0100

    Add systray

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,11 @@
 module github.com/intob/clutch/webserver
 
 go 1.21.4
+
+require fyne.io/systray v1.10.0
+
+require (
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,66 +1,38 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
 	"net/http"
+
+	"fyne.io/systray"
 )
-
-var (
-	rpcOrigin   string
-	rpcUser     string
-	rpcPassword string
-)
-
-func init() {
-	flag.StringVar(&rpcOrigin, "rpcorigin", "http://127.0.0.1:8332", "Origin of bitcoin rpc server, defaults to http://127.0.0.1:8332")
-	flag.StringVar(&rpcUser, "rpcuser", "", "User for bitcoin rpc server")
-	flag.StringVar(&rpcPassword, "rpcpassword", "", "Password for bitcoin rpc server")
-	flag.Parse()
-
-	fmt.Println("rpc origin: " + rpcOrigin)
-	fmt.Println("rpc user: " + rpcUser)
-	fmt.Println("rpc password: " + rpcPassword)
-	fmt.Println("listening on :6102")
-}
 
 func main() {
+	systray.Run(onReady, func() {})
+}
+
+func onReady() {
+	systray.SetTitle("Clutch")
+	systray.SetTooltip("Tiny bitcoin wallet")
+	addQuitItem()
+	serve()
+}
+
+func addQuitItem() {
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+	mQuit.Enable()
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
+	}()
+	systray.AddSeparator()
+}
+
+func serve() {
 	http.Handle("/rpc", http.HandlerFunc(handleRpc))
 	http.Handle("/", http.FileServer(http.Dir("../app")))
 	err := http.ListenAndServe(":6102", nil)
 	if err != nil {
 		fmt.Println("error: " + err.Error())
 	}
-}
-
-func handleRpc(w http.ResponseWriter, r *http.Request) {
-	coreReq, err := http.NewRequest("POST", rpcOrigin, r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("failed to build request: " + err.Error()))
-		return
-	}
-	if rpcUser != "" && rpcPassword != "" {
-		coreReq.SetBasicAuth(rpcUser, rpcPassword)
-	}
-	resp, err := http.DefaultClient.Do(coreReq)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	body, err := io.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("failed to read response body: " + err.Error()))
-		return
-	}
-	if resp.StatusCode != http.StatusOK {
-		w.WriteHeader(resp.StatusCode)
-		w.Write(body)
-		return
-	}
-	w.Write(body)
 }
